34aff98189868e3a235135e9afc4bf2311559b21
abdullahzen <abdullah.hzen@gmail.com>
2023-11-01 09:12:04 -0400

    Added a while true loop to demonstrate live development changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "helloworld"
+while true; do
+    echo "helloworld"
 
-echo "I will sleep now"
+    echo "I will sleep now"
 
-echo "somestuff from my container" > /persistent/file2
-# to mimick a server or a daemon like process that keeps running forever.
-sleep 10000
+    echo "somestuff from my container" > /persistent/file2
+    # to mimick a server or a daemon like process that keeps running forever.
+    sleep 3
+done
